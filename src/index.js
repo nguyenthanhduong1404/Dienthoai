3bb44b4a00d248c7eddce9e69ed3edd8fc4a14dd
nguyenthanhduong1404 <82082741+nguyenthanhduong1404@users.noreply.github.com>
2024-06-17 20:09:14 +0700

    add needed file

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+<script src="js/admin.js"></script>;
+<script src="js/chitietsanpham.js"></script>;
+<script src="js/classes.js"></script>;
+<script src="js/dungchung.js"></script>;
+<script src="js/giohang.js"></script>;
+<script src="js/lienhe.js"></script>;
+<script src="js/trangchu.js"></script>;
+<script src="js/nguoidung.js"></script>;
